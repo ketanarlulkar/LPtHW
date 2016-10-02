714ac30f2844d2f53f16189f56a3086c81a11aca
Ketan Arlulkar <ketan.arlulkar@gmail.com>
2016-10-02 12:14:16 +0530

    Excercise 10 Study Drills

--- ex10.py
+++ ex10.py
@@ -1,6 +1,7 @@
 tabby_cat = "\tI'm tabbed in."
 persian_cat = "I'm split\non a line."
 backslash_cat = "I'm \\ a \\ cat."
+myown_cat = "I'm split %r on a line."
 
 fat_cat = """
 I'll do a list:
@@ -13,3 +14,5 @@ print tabby_cat
 print persian_cat
 print backslash_cat
 print fat_cat
+# If I use %r none of the escape sequence will work
+print myown_cat % tabby_cat
