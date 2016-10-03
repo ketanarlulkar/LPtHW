66c37c7ddfa7aac5b972df31cc1c938551168feb
Ketan Arlulkar <ketan.arlulkar@gmail.com>
2016-10-03 09:43:03 +0530

    Excercise 11 Study Drills

--- ex11.py
+++ ex11.py
@@ -1,4 +1,7 @@
 print "How old are you?",
+# If the prompt argument is present, it is written to standard output
+# without a trailing newline. The function then reads a line from input,
+# converts it to a string (stripping a trailing newline), and returns that.
 age = raw_input()
 print "How tall are you?",
 height = raw_input()
