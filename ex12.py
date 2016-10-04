b9cc8d3ef9f97f314a90c010c14555c7cf0b5130
Ketan Arlulkar <ketan.arlulkar@gmail.com>
2016-10-04 09:14:18 +0530

    Excercise 12 Study Drills

--- ex12.py
+++ ex12.py
@@ -4,3 +4,14 @@ weight = raw_input("How much do you weigh? ")
 
 print "So, you're %r old, %r tall and %r heavy." % (
     age, height, weight)
+
+# Learn pydoc
+# Pydoc is a standard documentation module for the programming language Python
+# On linux type pydoc raw_input.
+# On Windows try python -m pydoc raw_input.
+
+# What's the difference between file and open in Python? When should I use which one?
+# When opening a file, it's preferable to use open() instead of
+# invoking this constructor directly. file is more suited to type testing
+# (for example, writing "isinstance(f, file)").
+# Also, file() has been removed since Python 3.0.
