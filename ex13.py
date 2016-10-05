dbb81184336b21c15256bef2dbe7b8301d21f646
Ketan Arlulkar <ketan.arlulkar@gmail.com>
2016-10-05 08:51:16 +0530

    Excercise 13 Study Drills

--- ex13.py
+++ ex13.py
@@ -1,3 +1,10 @@
+# Script will need exact 3 arguments to eexecute
+# If arguments are less than 3 we will get
+# ValueError: need more than 3 values to unpack
+# If arguments are more than 3 we will get
+# ValueError: too many values to unpack
+
+#Command line arguments are strings
 from sys import argv
 
 script, first, second, third = argv
