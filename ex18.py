d552c09ad1ef97111a95833d11441fa61fceef10
Ketan Arlulkar <ketan.arlulkar@gmail.com>
2016-10-15 22:00:31 +0530

    Excercise 18 Study Drills

--- ex18.py
+++ ex18.py
@@ -1,4 +1,8 @@
 # this one is like your scripts with argv
+# * tells Python to take all the arguments to the function and
+# then put them in args as a list.
+# It's like argv that you've been using, but for functions.
+# It's not normally used too often unless specifically needed.
 def print_two(*args):
     arg1, arg2 = args
     print "arg1: %r, arg2: %r" % (arg1, arg2)
@@ -15,7 +19,7 @@ def print_one(arg1):
 def print_none():
     print "I got nothin'."
 
-
+# "To 'run,' 'call,' or 'use' a function all mean the same thing."
 print_two("Zed","Shaw")
 print_two_again("Zed","Shaw")
 print_one("First!")
