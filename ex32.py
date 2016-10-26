17aa1c7591920d60d9e4e834ebfde8fa6bdc2b84
Ketan Arlulkar <ketan.arlulkar@gmail.com>
2016-10-26 21:23:22 +0530

    Excercise 32 Study Drills

--- ex32.py
+++ ex32.py
@@ -18,12 +18,25 @@ for i in change:
 # we can also build lists, first start with an empty one
 elements = []
 
+# This is a versatile function to create lists containing arithmetic progressions.
+# It is most often used in for loops. The arguments must be plain integers.
+# If the step argument is omitted, it defaults to 1.
+# If the start argument is omitted, it defaults to 0.
 # then use the range function to do 0 to 5 counts
 for i in range(0, 6):
     print "Adding %d to the list." % i
     # append is a function that lists understand
     elements.append(i)
 
+elements1 = []
+elements1 = range(0,6)
+
 # now we can print them out too
 for i in elements:
     print "Element was: %d" % i
+
+print elements1
+print elements
+
+# Other functions used on list are:
+# append, count, extend, index, insert, pop, remove, reverse, sort
