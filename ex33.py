752e0b319c27e9635ceb125ddeb21474dbd1a716
Ketan Arlulkar <ketan.arlulkar@gmail.com>
2016-10-27 18:31:29 +0530

    Excercise 33 Study Drills

--- ex33.py
+++ ex33.py
@@ -1,16 +1,22 @@
-i = 0
-numbers = []
 
-while i < 6:
-    print "At the top i is %d" % i
-    numbers.append(i)
+def while_list(max_no, step):
+    i = 0
+    numbers = []
 
-    i = i + 1
-    print "Numbers now: ", numbers
-    print "At the bottom i is %d" % i
+    while i < max_no:
+        print "At the top i is %d" % i
+        numbers.append(i)
 
+        i = i + step
+        print "Numbers now: ", numbers
+        print "At the bottom i is %d" % i
 
-print "The numbers: "
+        print "The numbers: "
 
-for num in numbers:
-    print num
+    for num in numbers:
+        print num
+
+while_list(1,1)
+while_list(2,2)
+while_list(4,2)
+while_list(8,2)
